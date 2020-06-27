8003e88889e70ea459c3d2c79f792cd1203e3e40
chickenbit <chickenbit@users.noreply.github.com>
2020-06-27 13:52:56 -0700

    joystick

--- setup/io.sh
+++ setup/io.sh
@@ -3,3 +3,6 @@ sudo apt-get install fswebcam
 
 # https://motion-project.github.io/
 sudo apt-get install motion
+
+# https://pimylifeup.com/xbox-controllers-raspberry-pi/
+sudo apt-get install joystick
