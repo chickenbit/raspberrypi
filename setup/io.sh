4f11e49df2e9d42b17357ce2f0cf7540d8377c60
chickenbit <chickenbit@users.noreply.github.com>
2020-07-18 12:12:15 -0700

    added jstest-gtk

--- setup/io.sh
+++ setup/io.sh
@@ -6,3 +6,10 @@ sudo apt-get install motion
 
 # https://pimylifeup.com/xbox-controllers-raspberry-pi/
 sudo apt-get install joystick
+# sudo jstest /dev/input/js0
+
+# visual for verifying and calibrating joysticks
+sudo apt-get install jstest-gtk
+
+# mapping joystick to commands: can use for pygame
+#sudo apt-get install qjoypad
